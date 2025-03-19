0645c58b2bc5f5cbd7e7a4a03fc9d6a057c666a6
Eseoghene94 <eseoghenedavid1@gmail.com>
2025-03-19 13:02:08 +0100

    new file:   vercel.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensures assets are loaded from the root URL
+  build: {
+    outDir: "dist", // Default output directory
+  },
 });
